36cc8cfd5994ee9933df0bb2d60f27838623179e
gylman <gylmanbm@gmail.com>
2024-10-11 16:02:25 +0900

    refactor: restructure, fix version issues, modularize

--- src/skde/wasm/wasm.rs
+++ src/skde/wasm/wasm.rs
@@ -1,9 +1,6 @@
 use serde_wasm_bindgen::{self, from_value};
+use skde::{decrypt as decryptor, encrypt as encryptor, PublicKey, SecretKey, SkdeParams};
 use wasm_bindgen::{prelude::*, JsValue};
-
-use crate::delay_encryption::{
-    decrypt as decryptor, encrypt as encryptor, PublicKey, SecretKey, SkdeParams,
-};
 
 #[wasm_bindgen]
 pub fn encrypt(
@@ -11,20 +8,15 @@ pub fn encrypt(
     message: JsValue,
     encryption_key: JsValue,
 ) -> Result<String, JsValue> {
+    // Deserialize the input values from JsValue to Rust types
     let skde_params: SkdeParams = from_value(skde_params).unwrap();
     let message: String = message.as_string().unwrap();
     let encryption_key: PublicKey = from_value(encryption_key).unwrap();
 
+    // Perform encryption and handle the result
     match encryptor(&skde_params, &message, &encryption_key) {
-        Ok(ciphertext) => {
-            // Convert the ciphertext to JsValue and then to a String
-            let ciphertext_str = ciphertext.to_string();
-            Ok(ciphertext_str)
-        }
-        Err(e) => {
-            // Handle the error and convert it to JsValue
-            Err(JsValue::from_str(&format!("Encryption error: {}", e)))
-        }
+        Ok(ciphertext) => Ok(ciphertext.to_string()), // Return ciphertext as a string
+        Err(e) => Err(JsValue::from_str(&format!("Encryption error: {}", e))), // Handle error case
     }
 }
 
@@ -34,17 +26,13 @@ pub fn decrypt(
     ciphertext: &str,
     decryption_key: JsValue,
 ) -> Result<String, JsValue> {
+    // Deserialize the input values from JsValue to Rust types
     let skde_params: SkdeParams = from_value(skde_params).unwrap();
     let decryption_key: SecretKey = from_value(decryption_key).unwrap();
 
+    // Perform decryption and handle the result
     match decryptor(&skde_params, ciphertext, &decryption_key) {
-        Ok(message) => {
-            // Convert the decrypted message to a string and return it
-            Ok(message)
-        }
-        Err(e) => {
-            // Handle the error and convert it to JsValue
-            Err(JsValue::from_str(&format!("Decryption error: {}", e)))
-        }
+        Ok(message) => Ok(message), // Return the decrypted message
+        Err(e) => Err(JsValue::from_str(&format!("Decryption error: {}", e))), // Handle error case
     }
 }
